0d313908e4fb0864ad08ba015a8a9b596003416c
Owen Kieffer-Jones <okj579@gmail.com>
2021-12-26 23:30:17 +0100

    better error handling and logging

--- deploy.js
+++ deploy.js
@@ -30,7 +30,7 @@ const requestWithAuth = octokit.request.defaults({
 
 (async () => {
     try {
-        console.log('Getting deployment data')
+        console.log(`Getting deployment data (id: ${deployment_id})`)
         const {data: deployment} = await requestWithAuth('GET /repos/{owner}/{repo}/deployments/{deployment_id}', {deployment_id});
 
         console.log('Setting deployment status to in_progress');
@@ -41,11 +41,19 @@ const requestWithAuth = octokit.request.defaults({
             environment
         });
 
-
         console.log('Getting run artifacts');
         const run_id = deployment.payload;
-        const {data: artifacts} = await requestWithAuth('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts', {run_id});
-        const artifact = artifacts.artifacts.find(x => x.name === 'dist');
+        const artifacts = await (async () => {
+            for (let x = 0; x < 5; x++) {
+                let {data} = await requestWithAuth('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts', {run_id});
+                if (data.total_count) return data.artifacts;
+                console.log('Retrying...');
+                await new Promise(res => setTimeout(res, 1500));
+            }
+            throw new Error('No artifacts found');
+        })()
+        const artifact = artifacts.find(x => x.name === 'dist');
+        if (!artifact) throw new Error('No artifact named dist');
 
         console.log('Getting artifact download URL');
         const res = await requestWithAuth('HEAD /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}', {
@@ -57,7 +65,7 @@ const requestWithAuth = octokit.request.defaults({
         });
         const download_url = res.headers.location;
 
-        console.log('Downloading artifact');
+        console.log(`Downloading artifact from ${download_url}`);
         await exec(`wget "${download_url}" -O build.zip`, {cwd: deploy_dir});
 
         console.log('Cleaning deployment directory');
@@ -75,6 +83,7 @@ const requestWithAuth = octokit.request.defaults({
 
     } catch (error) {
 
+        console.log(error);
         console.log('Setting deployment status to failure');
         await requestWithAuth('POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses', {
             deployment_id,
@@ -83,7 +92,6 @@ const requestWithAuth = octokit.request.defaults({
             environment
         });
 
-        console.log(error);
         process.exit(1);
     }
 
